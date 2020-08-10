51aa581b2e4e0fe6f3f1f489768c1c8a0190bf96
Manas Sharma <61354715+AfanasAbigor@users.noreply.github.com>
2020-08-10 16:55:15 +0600

    Create 141A - Amusing Joke.cpp

---
+++ 141A - Amusing Joke.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+    string a,b,c,d;
+    cin>>a>>b>>c;
+
+    d = a+b;
+
+    sort(d.begin(),d.end());
+    sort(c.begin(),c.end());
+
+    if(d==c)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
+//666Lucifer666
