d8a95e7b42e6f943d769108ffccce850baaaf5fc
Manas Sharma <61354715+AfanasAbigor@users.noreply.github.com>
2020-12-01 01:08:05 +0600

    Create 1455 C - Ping-pong.cpp

---
+++ 1455 C - Ping-pong.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,a1,a2,i;
+
+    cin>>n;
+
+    for(i=1; i<=n; i++)
+    {
+        cin>>a1>>a2;
+        cout<<a1-1<<" "<<a2<<endl;
+    }
+
+    return 0;
+}
