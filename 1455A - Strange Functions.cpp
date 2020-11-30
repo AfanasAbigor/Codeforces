f581ce438ff469109951f9776d73a92e12906a69
Manas Sharma <61354715+AfanasAbigor@users.noreply.github.com>
2020-12-01 00:28:48 +0600

    Create 1455A - Strange Functions.cpp

---
+++ 1455A - Strange Functions.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int i,n;
+    string s;
+    cin>>n;
+
+    for(i=1; i<=n; i++)
+    {
+
+        cin>>s;
+        cout<<s.size()<<endl;
+
+    }
+
+    return 0;
+
+}
